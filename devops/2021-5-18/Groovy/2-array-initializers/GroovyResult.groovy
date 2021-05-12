9271559f6d789f2d70c52057114b06a4693e7128
Jack <jack.zhe.wang@kingland.com>
2021-05-12 20:23:57 +0800

    Add groovy array initializers demo

---
+++ devops/2021-5-18/Groovy/2-array-initializers/GroovyResult.groovy
@@ -0,0 +1,9 @@
+int[] array1 = [1, 2, 3];
+println(array1);
+
+def array2 = new int[] {4, 5, 6};
+println(array2);
+
+// throw unexpected input exception
+// def array3 = {7, 8, 9};
+// println(array3);
